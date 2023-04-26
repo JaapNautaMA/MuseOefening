5fc5d381ffa852fa345f347fb67ab0623d490009
Jelle Sjollema <j.sjollema@ma-web.nl>
2023-04-26 13:52:59 +0200

    HelloWorld
    test

---
+++ Assets/Scripts/TestObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hallo wereld");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
